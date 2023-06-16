12e870f7e0d04c67295e60f25b21ee95979f946d
wonyongg <111210881+wonyongg@users.noreply.github.com>
2023-06-17 03:02:06 +0900

    6/17

--- react/first-react/src/index.js
+++ react/first-react/src/index.js
@@ -8,13 +8,14 @@ import reportWebVitals from './reportWebVitals';
 // import Library from './chapter_04/Library';
 // import CommentList from './chapter_05/CommentList';
 // import NotificationList from './chapter_06/NotificationList';
-import Accommodate from './chapter_07/Accommodate';
+// import Accommodate from './chapter_07/Accommodate';
+import ConfirmButtonFunction from './chapter_08/ConfirmBUtton-function';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); 
 // 리액트 18버전부터 렌더링 코딩 방식이 바뀜
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButtonFunction />
   </React.StrictMode>
 );
 
