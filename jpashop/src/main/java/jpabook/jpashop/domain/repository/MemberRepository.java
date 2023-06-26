97d20df356a2019d4733429b7b4c048851b6a3cf
wonyongg <111210881+wonyongg@users.noreply.github.com>
2023-06-27 02:34:03 +0900

    6/27

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+import java.util.List;
+
+@Repository// 이 애너테이션이 있으면 컴포넌트 스캔의 대상이 되어 자동으로 스프링 빈으로 등록이 된다.
+@RequiredArgsConstructor // em의 EntityManager 주입을 하려면 @PersistenceContext이 필요한데 스프링이 @Autowired도 되게 해주기 때문에 이 애너테이션 사용이 가능하다.
+public class MemberRepository {
+//    @PersistenceContext // EntityManager를 주입한다.
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id); // (타입,pk)
+    }
+
+    public List<Member> findAll() {
+
+        return em.createQuery("select m from Member  m", Member.class) // query문, 반환타입
+                .getResultList(); // List로 반환
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name) // 파라미터 name과 :name 바인딩
+                .getResultList();
+    }
+}
---
+++ jpashop/src/main/java/jpabook/jpashop/domain/service/MemberService.java
@@ -0,0 +1,50 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service // 이 애너테이션이 있으면 컴포넌트 스캔의 대상이 되어 자동으로 스프링 빈으로 등록이 된다.
+@Transactional // JPA의 데이터 변경이나 로직들은 가급적이면 트랙잭션 안에서 실행되어야 한다. default로 readOnly = false이며, 쓰기는 true로 하면 안된다.
+@RequiredArgsConstructor // final이 있는 필드만을 가지고 생성자를 만들어준다.
+public class MemberService {
+    private final MemberRepository memberRepository; // 초기값 세팅 후 변경 불가
+
+    // @Autowired 스프링 빈에 등록된 MemberRepository를 주입해준다. 최신 스프링에서는 안 넣어도 됨. (생성자 주입)
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//    @RequiredArgsConstructor 사용으로 주석
+
+    // 회원 가입
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+
+    }
+
+
+    // 회원 전체 조회
+    @Transactional(readOnly = true) // readOnly = true 옵션을 조회하는 메서드에서 사용하면 성능을 최적화한다.
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
