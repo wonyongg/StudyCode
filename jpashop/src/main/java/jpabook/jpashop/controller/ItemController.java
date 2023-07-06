e3c427658f494f5622c67a7e14f19f520ce2ceec
wonyongg <111210881+wonyongg@users.noreply.github.com>
2023-07-07 01:07:08 +0900

    7/7

--- jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -63,17 +63,19 @@ public class ItemController {
     }
 
     @PostMapping("items/{itemId}/edit")
-    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form) {
+    public String updateItem(@PathVariable Long itemId, @ModelAttribute("form") BookForm form) {
 
-        Book book = new Book();
-        book.setId(form.getId());
-        book.setName(form.getName());
-        book.setPrice(form.getPrice());
-        book.setStockQuantity(form.getStockQuantity());
-        book.setAuthor(form.getAuthor());
-        book.setIsbn(form.getIsbn());
+//        Book book = new Book();
+//        book.setId(form.getId());
+//        book.setName(form.getName());
+//        book.setPrice(form.getPrice());
+//        book.setStockQuantity(form.getStockQuantity());
+//        book.setAuthor(form.getAuthor());
+//        book.setIsbn(form.getIsbn());
+//        itemService.saveItem(book); 병합 방식
 
-        itemService.saveItem(book);
+
+        itemService.updateItem(itemId, form.getName(), form.getPrice(), form.getStockQuantity());
         return "redirect:/items";
     }
 }
--- jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -20,6 +20,26 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     */
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); // db에서 가져옴으로써 영속성 컨텍스트의 관리를 받음
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        // 변경사항이 있으므로 트랜잭션이 끝나고 커밋 시 변경을 감지하여(dirty checking) 변경된 부분만 업데이트 쿼리가 나감
+    }
+
+    /**
+     * 병합(merge) 동작 방식
+     * 준영속 엔티티의 식별자 값으로 영속 엔티티를 조회한다.
+     * 영속 엔티티의 값을 준영속 엔티티 값으로 병합한다.
+     * 이때 값이 없는 필드는 null로 업데이트된다.(병합은 모든 필드를 교체하기 떄문이다.)
+     * 트랜잭션 커밋 시 변경 감지가 동작해 데이터베이스에 업테이트 쿼리가 나간다.
+     */
+
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
