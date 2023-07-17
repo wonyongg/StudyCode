b9484d4eb2b568f9505eb73bdccbf4755acb47b2
wonyongg <111210881+wonyongg@users.noreply.github.com>
2023-07-18 01:26:33 +0900

    7/18

--- docker/kub-action-01-starting-setup/app.js
+++ docker/kub-action-01-starting-setup/app.js
@@ -4,8 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => {
   res.send(`
-    <h1>Hello from this NodeJS app!</h1>
-    <p>Try sending a request to /error and see what happens</p>
+    <h1>hwangwonyong's kubernetes Test!</h1>
+    <p>hehehe</p>
   `);
 });
 
