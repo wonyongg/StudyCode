9c17589ae43e4162d55af0fbde78da1976390d95
wonyongg <111210881+wonyongg@users.noreply.github.com>
2023-12-01 02:54:28 +0900

    12/1

---
+++ Inflearn/스프링 핵심원리 - 기본편/core/src/test/java/hello/core/beanFind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,27 @@
+package hello.core.beanFind;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.order.RateDiscountPolicy;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ApplicationContextExtendsFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+
+    @Configuration
+    static class TestConfig {
+
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy() {
+            return new FixDiscountPolicy();
+        }
+    }
+}
