c229fa2e23d8a442f6d6d304e67d51d79b1daab9
wonyongg <111210881+wonyongg@users.noreply.github.com>
2024-01-10 00:00:39 +0900

    1/10

--- Inflearn/스프링 핵심원리 - 기본편/core/src/main/java/hello/core/web/LogDemoService.java
+++ Inflearn/스프링 핵심원리 - 기본편/core/src/main/java/hello/core/web/LogDemoService.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +10,10 @@ import org.springframework.stereotype.Service;
 public class LogDemoService {
 
     private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
+
     public void logic(String id) {
+//        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
