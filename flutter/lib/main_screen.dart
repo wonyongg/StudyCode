4c734d29a0f7feca983149134a371bece9e62193
wonyongg <111210881+wonyongg@users.noreply.github.com>
2024-07-26 01:30:36 +0900

    MyBusinessCard 프젝 완료

--- flutter/lib/main_screen.dart
+++ flutter/lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,8 +9,20 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  //자기소개
+  // 자기소개의 내용을 입력 받는 컨트롤러
   TextEditingController introduceController = TextEditingController();
+
+  // 자기소개 수정 모드 상태
+  bool isEditMode = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 위젯이 처음 실행될 때 호출된다.
+    getIntroduceData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -150,34 +163,80 @@ class _MainScreenState extends State<MainScreen> {
               ),
             ),
             // 자기소개 입력 필드
-            Container(
-              margin: EdgeInsets.only(left: 16, top: 16),
-              child: Text(
-                '자기소개',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween, // 양쪽으로 분할
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 16, top: 16),
+                  child: Text(
+                    '자기소개',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
+                GestureDetector(child: Container(
+                  margin: EdgeInsets.only(right: 16, top: 16),
+                  child: Icon(
+                    Icons.mode_edit_outline_outlined,
+                    color: isEditMode == true ? Colors.orange : Colors.teal,
+                    size: 24,
+                  ),
+                ), onTap: () async {
+                  if (isEditMode == false) {
+                    setState(() { // setState를 통해 위젯을 재빌드함(업데이트)
+                      isEditMode = true;
+                    });
+                  } else {
+                    if (!introduceController.text.isEmpty) {
+                      // 수정이 끝나면 저장 로직 시작
+                      var sharedPref = await SharedPreferences.getInstance();
+                      sharedPref.setString(
+                          'introduce', introduceController.text);
+
+                      setState(() {
+                        isEditMode = false;
+                      });
+                    } else {
+                      // snackBar 메시지로 입력값을 넣도록 알림
+                      var snackBar = SnackBar(
+                        content: Text('자기소개 입력 값이 비어있습니다.'),
+                        duration: Duration(seconds: 3),
+                      );
+
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                  }
+                },),
+              ],
             ),
             Container(
               margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: TextField(
                 maxLines: 5,
                 controller: introduceController,
+                enabled: isEditMode, // editMode를 통해 자기소개 입력창 활성화/비활성화 결정
                 decoration: InputDecoration(
                     border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    color: Color(0xffD9D9D9),
-                  ),
-                )),
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        color: Color(0xffD9D9D9),
+                      ),
+                    )),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> getIntroduceData() async {
+    // 기존에 저장된 자기소개 데이터가 있으면 가져오기
+    var sharedPref = await SharedPreferences.getInstance();
+    String introduceString = sharedPref.getString('introduce').toString();
+    introduceController.text = introduceString ?? ""; // null 합류 연산자
   }
 }
 // <a href="https://www.flaticon.com/kr/free-icons/" title="정체 아이콘">정체 아이콘 제작자: Freepik - Flaticon</a>
