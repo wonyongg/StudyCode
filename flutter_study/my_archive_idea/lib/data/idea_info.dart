d6e9e0e60ffc28aeb039401dcd856f363111088a
wonyongg <111210881+wonyongg@users.noreply.github.com>
2024-07-29 02:30:29 +0900

    idea_info class 생성

---
+++ flutter_study/my_archive_idea/lib/data/idea_info.dart
@@ -0,0 +1,45 @@
+class IdeaInfo {
+  int? id;
+  String title; // 아이디어 제목
+  String motive; // 작성 계기
+  String content; // 아이디어 내용
+  int priority; // 아이디어 중요도 점수
+  String feedback; // 유저 피드백 사항
+  int createdAt; // 생성 일시 (년월일시분으로 표시)
+
+  IdeaInfo({
+    this.id,
+    required this.title, // 메서드 호출 시기에 반드시 값이 포함되어야 함
+    required this.motive,
+    required this.content,
+    required this.priority,
+    required this.feedback,
+    required this.createdAt,
+  });
+
+  // IdeaInfo 객체를 Map 객체로 변환
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'priority': priority,
+      'feedback': feedback,
+      'createdAt': createdAt,
+    };
+  }
+
+  // Map 객체를 IdeaInfo 객체로 변환
+  // factory : 싱글톤 패턴처럼 인스턴스를 계속 생성하지 않고 공유하여 돌려씀
+  factory IdeaInfo.fromMap(Map<String, dynamic> map) {
+    return IdeaInfo(
+      id: map['id'],
+      title: map['title'],
+      motive: map['motive'],
+      content: map['content'],
+      priority: map['priority'],
+      feedback: map['feedback'],
+      createdAt: map['createdAt'],
+    );
+  }
+}
